bd09d1794558e5f0d17aeb5d52b3d632bb3bb0ae
Lisa <122068169+LisaMandarin@users.noreply.github.com>
2025-01-23 02:03:01 +0800

    fix login-form

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,18 +1,17 @@
 'use client';
-
+ 
 import { lusitana } from '@/app/ui/fonts';
 import {
   AtSymbolIcon,
-  ExclamationCircleIcon,
   KeyIcon,
-  
+  ExclamationCircleIcon,
 } from '@heroicons/react/24/outline';
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
-import { Button } from './button';
+import { Button } from '@/app/ui/button';
 import { useActionState } from 'react';
 import { authenticate } from '@/app/lib/actions';
 import { useSearchParams } from 'next/navigation';
-
+ 
 export default function LoginForm() {
   const searchParams = useSearchParams();
   const callbackUrl = searchParams.get('callbackUrl') || '/dashboard';
@@ -20,8 +19,9 @@ export default function LoginForm() {
     authenticate,
     undefined,
   );
+ 
   return (
-    <form className="space-y-3" action={formAction}>
+    <form action={formAction} className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
         <h1 className={`${lusitana.className} mb-3 text-2xl`}>
           Please log in to continue.
@@ -67,12 +67,16 @@ export default function LoginForm() {
             </div>
           </div>
         </div>
-        <input type='hidden' name='redirectTo' value={callbackUrl}/>
+        <input type="hidden" name="redirectTo" value={callbackUrl} />
         <Button className="mt-4 w-full" aria-disabled={isPending}>
           Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
         </Button>
-        <div className="flex h-8 items-end space-x-1" aria-live='polite' aria-atomic='true'>
-        {errorMessage && (
+        <div
+          className="flex h-8 items-end space-x-1"
+          aria-live="polite"
+          aria-atomic="true"
+        >
+          {errorMessage && (
             <>
               <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
               <p className="text-sm text-red-500">{errorMessage}</p>
